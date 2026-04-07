41d3752eb1773e060d0152c97e8be88f6c05dd3b
A Student <student@nowhere.edu>
2026-04-07 01:40:36 +0000

    Apr 6, 2026, 6:40 PM

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
 
   int cup = tbsp / 16;
   
-  tbsp = NumTsp % 16;
+  leftOver= NumTsp % 16;
   cout << tbsp << " Tablespoon(s)" << endl;
   cout << cup << " Cup(s)" << endl;
   //int quart = cup / 4;
