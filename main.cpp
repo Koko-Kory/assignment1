8fb203dcf6519cd5d9cae202ff9ca2ae66f075be
A Student <student@nowhere.edu>
2026-04-07 04:53:14 +0000

    Apr 6, 2026, 9:53 PM

--- main.cpp
+++ main.cpp
@@ -16,6 +16,6 @@ int main() {
 
 
   cout << tbsp << " Tablespoon(s)" << endl << leftOverTsp << " tsp" << endl;
-  cout << cup << " cups" << endlleftOverTbsp << " leftovertbsp" << endl;
+  cout << cup << " cups" << endl << leftOverTbsp << " leftovertbsp" << endl;
 
 }
